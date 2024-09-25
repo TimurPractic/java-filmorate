25a704c6d40c36aac693b858cfac821aa211a9cd
TimurPractic <timurowitch@yandex.ru>
2024-09-25 23:53:08 +0300

    Second Commit - new service for Film and User

---
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -0,0 +1,46 @@
+package ru.yandex.practicum.filmorate.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Service
+public class FilmService {
+
+    private final FilmStorage filmStorage;
+
+    @Autowired
+    public FilmService(FilmStorage filmStorage) {
+        this.filmStorage = filmStorage;
+    }
+
+    // Добавление лайка
+    public void addLike(int filmId, int userId) {
+        Film film = filmStorage.getFilmById(filmId).orElseThrow(() -> new IllegalArgumentException("Film not found"));
+
+        film.getLikes().add(userId);
+    }
+
+    // Удаление лайка
+    public void removeLike(int filmId, int userId) {
+        Film film = filmStorage.getFilmById(filmId).orElseThrow(() -> new IllegalArgumentException("Film not found"));
+
+        film.getLikes().remove(userId);
+    }
+
+    // Вывод 10 наиболее популярных фильмов по количеству лайков
+    public List<Film> getMostPopularFilms(int count) {
+        return filmStorage.getAllFilms().stream()
+                .sorted((f1, f2) -> Integer.compare(f2.getLikes().size(), f1.getLikes().size()))
+                .limit(count)
+                .collect(Collectors.toList());
+    }
+
+    public List<Film> getAllFilms() {
+        return filmStorage.getAllFilms();
+    }
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -0,0 +1,56 @@
+package ru.yandex.practicum.filmorate.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.user.UserStorage;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Service
+public class UserService {
+
+    private final UserStorage userStorage;
+
+    @Autowired
+    public UserService(UserStorage userStorage) {
+        this.userStorage = userStorage;
+    }
+
+    // Добавление в друзья
+    public void addFriend(int userId, int friendId) {
+        User user = userStorage.getUserById(userId).orElseThrow(() -> new IllegalArgumentException("User not found"));
+        User friend = userStorage.getUserById(friendId).orElseThrow(() -> new IllegalArgumentException("Friend not found"));
+
+        user.getFriends().add(friendId);
+        friend.getFriends().add(userId);
+    }
+
+    // Удаление из друзей
+    public void removeFriend(int userId, int friendId) {
+        User user = userStorage.getUserById(userId).orElseThrow(() -> new IllegalArgumentException("User not found"));
+        User friend = userStorage.getUserById(friendId).orElseThrow(() -> new IllegalArgumentException("Friend not found"));
+
+        user.getFriends().remove(friendId);
+        friend.getFriends().remove(userId);
+    }
+
+    // Вывод списка общих друзей
+    public List<User> getCommonFriends(int userId, int otherUserId) {
+        User user = userStorage.getUserById(userId).orElseThrow(() -> new IllegalArgumentException("User not found"));
+        User otherUser = userStorage.getUserById(otherUserId).orElseThrow(() -> new IllegalArgumentException("Other user not found"));
+
+        Set<Integer> commonFriendsIds = new HashSet<>(user.getFriends());
+        commonFriendsIds.retainAll(otherUser.getFriends());
+
+        return commonFriendsIds.stream()
+                .map(id -> userStorage.getUserById(id).orElse(null))
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
+    }
+
+    public List<User> getAllUsers() {
+        return userStorage.getAllUsers();
+    }
+}
